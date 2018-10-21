e674213de9ada21f6df1750cb566b8951959b4fc
Guilherme Braga <gbp1999@gmail.com>
2018-10-21 12:30:31 -0300

    boii
    Adicionados o yeahboi e o melhor programa de calouro da história, o THICCBOII, do @pedrovrc

---
+++ THICCBOII.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+typedef struct BIG{
+	unsigned long long q, w, e, r, t, y, u, i, o, p, a, s, d, f, g, h, j, k, l, z, x, c, v, b, n, m;
+} THICC_BOII;
+
+void main(){
+
+while(1){
+	malloc(sizeof(THICC_BOII)); 
+}
+
+}
---
+++ yeahboii.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main() {
+
+  //int i;
+/*
+  while(1){
+  printf("Y");
+  i++;
+  if(i == 100)
+  break;
+  }
+
+  while(1){
+  printf("E");
+  i++;
+  if(i == 500)
+  break;
+  }
+
+  while(1){
+  printf("A");
+  i++;
+  if(i == 200)
+  break;
+  }
+
+  while(1){
+  printf("H");
+  i++;
+  if(i == 50)
+  break;
+  }
+
+  printf("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+*/
+  while(1){
+    printf("ZAP ");
+  }
+  return 0;
+}
